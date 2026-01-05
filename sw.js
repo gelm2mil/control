f8657c6f9816b9babaf724eb89a9bd8cdbfe8788
gelm2mil <nerox2mil@proton.me>
2026-01-04 21:29:19 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "gelm-control-v1";
+
+const FILES_TO_CACHE = [
+  "/control/",
+  "/control/index.html",
+  "/control/styles.css",
+  "/control/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
